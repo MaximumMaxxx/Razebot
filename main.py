939e1d7ffe7045faf91a8f8378cf5d9e6d2d3d5a
MaximumMaxxx <77695343+MaximumMaxxx@users.noreply.github.com>
2021-11-09 17:06:48 -0800

    Added arg support to ">rc"

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ import discord
 from discord import embeds
 from discord.ext import commands
 import random
+from mysql.connector import connect
 import requests
 import re
 from PIL import ImageColor
+import logging
 
 # A very overkill solution to caching common usernames however, it works and is good SQL practice.
 # Obviously only 
 UseSQL = False
 if UseSQL == True:
     import mysql.connector
-    DB = mysql.connector.connect()
+    DB = mysql.connector.connect("Your DB connection")
     print(DB)
 
-
+settings = {"assumed region":"na",}
 
 bot = commands.Bot(command_prefix='>')
 
@@ -25,7 +27,7 @@ def save_to_DB(user, username):
     print(username)
 
     cursor = DB.cursor()
-    sql = f"CREATE TABLE IF NOT EXISTS {user} (quantity INT, ign VARCHAR(255))"
+    sql = f"CREATE TABLE IF NOT EXISTS {user} (id INT, username TEXT, quantity INT, ign VARCHAR(255))"
     cursor.execut(sql)
     sql = f"SELECT * FROM {user} WHERE text LIKE {username} LIMIT 1"
     cursor.execute(sql)
@@ -50,27 +52,43 @@ async def ping(ctx):
 async def rand(ctx):
     await ctx.send(str(random.randint(0,100)))
 
+
 @bot.command()
-async def rc(ctx):  
-    try:  
-        # Input validation
-        def check(msg):
-            return msg.author == ctx.author and msg.channel == ctx.channel
+async def rc(ctx,*arg):  
+    try:
+        # allows support for the original call setup
+        if len(arg)== 0:
+            # Input validation
+            def check(msg):
+                return msg.author == ctx.author and msg.channel == ctx.channel
 
-        # Input from the user
-        await ctx.send("What region? na,eu,ap,kr")
-        region = await bot.wait_for("message", check=check, timeout=30)
+            # Input from the user
+            await ctx.send("What region? na,eu,ap,kr")
+            region = await bot.wait_for("message", check=check, timeout=30)
+            region =region.content
+            
+            await ctx.send("What Username and tag? Ex: Bob#6969")
+            name= await bot.wait_for("message", check=check, timeout=30)
+            name = name.content
+
+            # Weird Regex thing to split all non letters and numbers
+        elif len(arg) == 1:
+            name = arg[0]
+            region = settings["assumed region"]
+            
+        else:
+            print("2 args")
+            name= arg[0]
+            region = arg[1]
+            
+            
+        split = name.split('#')
         
-        await ctx.send("What Username and tag? Ex: Bob#6969")
-        name= await bot.wait_for("message", check=check, timeout=30)
-
-        # Weird Regex thing to split all non letters and numbers
-        split = re.findall(r"[\w']+", name.content)
+        
 
         # Api calls, using formatted strings
         CompTiers = requests.get("https://valorant-api.com/v1/competitivetiers")
-        MMR= requests.get(f"https://api.henrikdev.xyz/valorant/v1/mmr/{region.content}/{split[0]}/{split[1]}")
-        profile = requests.get(f"https://api.henrikdev.xyz/valorant/v1/account/{split[0]}/{split[1]}")
+        MMR= requests.get(f"https://api.henrikdev.xyz/valorant/v1/mmr/{region}/{split[0]}/{split[1]}")
 
         # Some default values in case a 200 isn't recieved
         default_color = False
@@ -101,7 +119,7 @@ async def rc(ctx):
                 output = "Something broke. Oh no!"
                 # Not sure how actually useful this is, but it exists
                 with open('log.txt','w') as f:
-                    f.write(f"Error in rc function {MMR.status_code} | {time.ctime(time.time())} | {region.content}/{split[0]}/{split[1]} | {MMR.json()} \n")
+                    f.write(f"Error in rc function {MMR.status_code} | {time.ctime(time.time())} | {region}/{split[0]}/{split[1]} | {MMR.json()} \n")
             EMB_color = discord.Color.red()
             default_color = True
             
@@ -118,20 +136,19 @@ async def rc(ctx):
 
         if MMR.status_code==200:
             #add some stuff that we couldn't make before because of being required to make the embed
-            item = discord.Embed(title=name.content, description=f"The stats and rank for {name.content}" ,color=EMB_color)
+            item = discord.Embed(title=name, description=f"The stats and rank for {name}" ,color=EMB_color)
             item.add_field(name="Rank",value=rank,inline=True)
             item.add_field(name="MMR",value=elo,inline=True)
         else:
-            item = discord.Embed(title=name.content, description=output, color=EMB_color)
+            item = discord.Embed(title=name, description=output, color=EMB_color)
 
         item.set_thumbnail(url=image)
         item.set_footer(text="Razebot by MaximumMaxx")
         await ctx.send(embed = item)
-
         # Upon successful completion aka getting the embed output
 
         if UseSQL == True:
-            save_to_DB(ctx.author.id,name.content)
+            save_to_DB(ctx.author.id,name)
 
     except TimeoutError:
         embed = discord.Embed(title="Timeout, took too long", description=f"A 30 second timeout ran out." ,color=discord.Color.red())
@@ -139,5 +156,6 @@ async def rc(ctx):
     except:
         embed = discord.Embed(title="Something broke in a very bad way", description=f"Please send help" ,color=discord.Color.red())
         await ctx.send(embed = embed)
+        print(logging.exception(''))
 
-bot.run('')
+bot.run('Bot Token')
