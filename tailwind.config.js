61246a099d96f32317171e1cd108fbb1a0f9d923
MaximumMaxx <maxwellwindland@gmail.com>
2022-01-02 06:40:58 -0800

    Added tailwind config]

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["../templates/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'valored':'#fa4454',
+        'mainbg':'rgb(0,0,0)',
+        'valoreddarker':'#dc3d4b',
+        'valoreddarkerer':'#B10023'
+      },
+    },
+  },
+  plugins: [],
+  
+}
