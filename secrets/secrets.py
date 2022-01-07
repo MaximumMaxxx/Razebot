96d27bb164f9a9d8387ed7d1f563f5118042ece7
MaximumMaxx <maxwellwindland@gmail.com>
2022-01-06 16:38:59 -0800

    secrets is not a module to make life easier

---
+++ secrets/secrets.py
@@ -0,0 +1,16 @@
+# This file holds all the environment variables
+# Technically secrets isn't the correct word but it's a good enough descriptor
+from dataclasses import dataclass
+
+Secrets= {
+    "bottoken":"",
+    "dscclientid":"",
+    "dscclientsecret":"",
+    "dscredirecturi":"",
+
+    "dbuname":"",
+    "dbpassword":"",
+    "database":"",
+    "dbhost":"",
+    "websecretkey":"",
+}
