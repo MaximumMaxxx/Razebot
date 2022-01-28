b6d1ce754972737043896b71a53a45c62eed24bb
MaximumMaxxx <77695343+MaximumMaxxx@users.noreply.github.com>
2022-01-28 00:57:17 -0800

    Atleast RankCheck Account works now

--- bot/rankCheck.py
+++ bot/rankCheck.py
@@ -1,6 +1,7 @@
 import math
 import asyncio
 import logging
+from random import choice
 
 import discord
 from discord.ext import commands
@@ -26,9 +27,13 @@ class Rankcheck(commands.Cog):
         # Getting the actual account details
         region, acc, tag = account[2], account[1], account[3]
         try:
-            uuid = requests.get(
-                f"https://api.henrikdev.xyz/valorant/v1/account/{acc}/{tag}").json()['puuid']
-        except discord.ApplicationCommandInvokeError as exception:
+            accountReq = requests.get(
+                f"https://api.henrikdev.xyz/valorant/v1/account/{acc}/{tag}")
+            if accountReq.status_code != 200:
+                return(discord.Embed(title="ERROR", description="Account not found"))
+            else:
+                puuid = accountReq.json()["data"]['puuid']
+        except discord.ApplicationCommandInvokeError or KeyError as exception:
             logging.exception(
                 f"Failed to retrieve account details with url: https://api.henrikdev.xyz/valorant/v1/account/{acc}/{tag} produces error: {exception}")
             return(discord.Embed(title="ERROR", description="Account not found", color=discord.Color.red()))
@@ -44,12 +49,16 @@ class Rankcheck(commands.Cog):
             mmr_change = 0
             for game in MMR_json["data"]:
                 mmr_change += game["mmr_change_to_last_game"]
-
+            if len(MMR_json["data"]) == 0:
+                return discord.Embed(title="ERROR", description="No data avaliable for that player. Double check your spelling and try again.", color=discord.Color.red())
             rank = MMR_json["data"][0]["currenttierpatched"]
             tiernum = MMR_json["data"][0]["currenttier"]
             elo = MMR_json["data"][0]["elo"]
-            image = self.CompTiers.json(
+            image: str = self.CompTiers.json(
             )["data"][0]["tiers"][tiernum]["largeIcon"]
+            print(image)
+            print(type(image))
+
             color = self.CompTiers.json(
             )["data"][0]["tiers"][tiernum]["color"][:6]
             embed = discord.Embed(color=discord.Color.from_rgb(ImageColor.getcolor("#"+color, "RGB")[0], ImageColor.getcolor(
@@ -60,9 +69,9 @@ class Rankcheck(commands.Cog):
             return(discord.Embed(title="ERROR", description="Not enough recent data or wrong region", color=discord.Color.red()))
         elif MMR.status_code == 429:
             return(discord.Embed(title="ERROR", description="The bot has been rate limited. Please try again in a few minutes", color=discord.Color.red()))
-        # Python devs be like:
         else:
-            logging.error(MMR.json())
+            logging.error(
+                f"{MMR.json()} error while retrieve data region: {region} username: {acc} tag: {tag}")
             return(discord.Embed(title="ERROR", description="Something went horribly wrong and we are all going to die.", color=discord.Color.red()))
 
         if MH.status_code == 200:
@@ -72,7 +81,7 @@ class Rankcheck(commands.Cog):
 
             for i in range(match_count):
                 for j in range(len(MH_json["data"][i]["players"]["all_players"])):
-                    if MH_json["data"][i]["players"]["all_players"][j]["puuid"] == uuid:
+                    if MH_json["data"][i]["players"]["all_players"][j]["puuid"] == puuid:
                         player_index = j
                         break
                 kills += MH_json["data"][i]["players"]["all_players"][player_index]["stats"]["kills"]
@@ -84,15 +93,15 @@ class Rankcheck(commands.Cog):
             assists = round(assists/match_count, 1)
             score = round(score / match_count, 1)
 
-            embed.add_field(name="Stats from the last 5 gmaes",
-                            value=f"KDA: {kills}|{deaths}|{assists} \n KDR: {kills/deaths} \n Score: {score} \n MMR Change: {mmr_change}")
+            embed.add_field(name="Average stats from the last 5 games",
+                            value=f"KDA: {kills} | {deaths} | {assists} \n KDR: {round(kills/deaths,2)} \n Score: {score} \n MMR Change: {mmr_change}")
         else:
             embed.add_field(
                 name="ERROR", value="Error getting other stats. If the issue persists please contact me @ MaximumMaxx#0001")
             logging.error(
                 f"Failure retrieving Match History data: {MH.json()}")
-        embed.set_image(image)
-        embed.set_footer("Razebot by MaximumMaxx")
+        embed.set_image(url=image)
+        embed.set_footer(text="Razebot by MaximumMaxx")
         return(embed)
 
     @commands.slash_command(name="rankchecklist", description="Check the Rank of an account from either your Quick accounts or My accounts list")
@@ -196,15 +205,24 @@ class Rankcheck(commands.Cog):
                 # ending the loop if user doesn't react after x seconds
         await ctx.respond("Working on that ... please wait")
         msg = await ctx.interaction.original_message()
-        await msg.edit(embed=self.get_acc(accounts[account_index]))
+        await msg.edit(content=None, embed=self.get_acc(accounts[account_index]))
 
     @commands.slash_command(name="rankcheckaccount", description="Get the stats for a specific VALORANT account")
-    async def rcacc(self, ctx: discord.ApplicationContext, account: str = option(name="Account", description="The VALORANT account you would like to check the rank of"), region: str = option(name="Region", description=f"The region the account is in. If not specified will default to the server's default region.", choices=["Na", "Eu"])):
+    async def rcacc(self, ctx: discord.ApplicationContext, account: str = option(name="Account", description="The VALORANT account you would like to check the rank of"), region: str = option(name="Region", description=f"The region the account is in. If not specified will default to the server's default region.", choices=[discord.OptionChoice(name="North America", value="na"), discord.OptionChoice(name="Europe", value="eu"), discord.OptionChoice(name="Korea", value="kr"), discord.OptionChoice(name="Asia Pacific", value="ap")])):
         # The formatting is a little wack but it does the thing hopefully and it's one line so I'll take the jank
         # Accounts is expected to be a list of tuples but you can just pass in a one item list and 0 and it acomplishes the same thing
+
         await ctx.respond("Working on that ... please wait")
+        if type(region) is not str:
+            with engine.connect() as conn:
+                result = conn.execute(
+                    text(
+                        f"SELECT * FROM set{ctx.guild.id} WHERE setting = 'region'")
+                )
+                region = result.all()[0][2]
+
         msg = await ctx.interaction.original_message()
-        await msg.edit(embed=self.get_acc((None, account.split("#")[0], region,  account.split("#")[1])))
+        await msg.edit(content=None, embed=self.get_acc((None, account.split("#")[0], region,  account.split("#")[1])))
 
 
 def setup(bot):
