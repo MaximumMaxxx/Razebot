15fd187458f565faaa82cc20df1b7b90bad180d5
Max <maxwellwindland@gmail.com>
2022-03-15 22:45:08 -0700

    Changed secrets a bit

--- secrets/secrets.py
+++ secrets/secrets.py
@@ -1,16 +1,22 @@
 # This file holds all the environment variables
 # Technically secrets isn't the correct word but it's a good enough descriptor
-from dataclasses import dataclass
+import time
 
-Secrets= {
-    "bottoken":"",
-    "dscclientid":"",
-    "dscclientsecret":"",
-    "dscredirecturi":"",
 
-    "dbuname":"",
-    "dbpassword":"",
-    "database":"",
-    "dbhost":"",
-    "websecretkey":"",
-}
+class Secrets():
+    def __init__(self) -> None:
+        pass
+
+    bottoken = ""
+    dscclientid = ""
+    dscclientsecret = ""
+    dscredirecturi = ""
+
+    dbuname = ""
+    dbpassword = ""
+    database = ""
+    dbhost = ""
+
+    websecretkey = b""
+    webhost = ""
+    webport = 6969
