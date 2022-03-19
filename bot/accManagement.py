9ec9966fcd4f32dcdb71a8201e98f466a1807a94
Max <maxwellwindland@gmail.com>
2022-03-19 12:24:50 -0700

    Abstracted account management

--- bot/accManagement.py
+++ bot/accManagement.py
@@ -5,24 +5,26 @@ from sqlalchemy import create_engine, text
 import requests
 
 from secrets.secrets import Secrets
-from helpers.Helper import CreateAccTable, regions, AddAcc, RmAcc, regionsChoice
+from helpers.Helper import CreateAccTable, validRanks
+from helpers.accHelpers import addHelper, removeHelper, listHelper
 
 
 engine = create_engine(
-    f"mysql+pymysql://{Secrets.dbuname}:{Secrets.dbpassword}@{Secrets.dbhost}/{Secrets.database}", echo=True, future=True)
+    f"mysql+pymysql://{Secrets.dbuname}:{Secrets.dbpassword}@{Secrets.dbhost}/{Secrets.database}", echo=Secrets.echo, future=Secrets.future)
 
 
 class accManagement(commands.Cog):
     def __init__(self, bot: discord.bot.Bot) -> None:
         self.bot = bot
+
     myaccs = discord.SlashCommandGroup(
-        "MyAccounts", "The commands for managing your owned accounts")
+        "myaccounts", "The commands for managing your owned accounts")
 
     quickaccs = discord.SlashCommandGroup(
-        "QuickAccounts", "The commands for managing your quick accounts")
+        "quickaccounts", "The commands for managing your quick accounts")
 
     @commands.slash_command(name="updaterole", description="Update your server role based on the ranks of the accounts saved in your myaccs list")
-    async def updaterole(self, ctx):
+    async def updaterole(self, ctx: discord.ApplicationContext):
         await ctx.respond("Thinking <:loadingbutbetter:914754938175893524>")
         with engine.connect() as conn:
             result = conn.execute(
@@ -30,17 +32,19 @@ class accManagement(commands.Cog):
             )
             guild_roles = result.all()
             # Check if the server has rank roles configured
-            if len(guild_roles) == len(self.valid_ranks()):
+            if len(guild_roles) == len(validRanks()):
 
                 # Convert roles into a dictionary
                 roleDict = {}
                 for i in range(len(guild_roles)):
                     roleDict[guild_roles[i][1]] = guild_roles[i][2]
 
+                print(roleDict)
+
                 for rank in roleDict:
                     await ctx.author.remove_roles(discord.utils.get(ctx.author.guild.roles, id=int(roleDict[rank])))
 
-                CreateAccTable(engine, ctx.message.author.id, "M")
+                CreateAccTable(engine, ctx.author.id, "M")
                 result = conn.execute(
                     text(f"select * from M{ctx.author.id}")
                 )
@@ -96,124 +100,63 @@ class accManagement(commands.Cog):
 
     @myaccs.command()
     async def list(self, ctx):
-        CreateAccTable(engine, ctx.author.id, "M")
-        with engine.connect() as conn:
-            result = conn.execute(
-                text(f"SELECT * FROM M{ctx.author.id}")
-            )
-            author_accs = result.all()
-
-            if len(author_accs) == 0:
-                embed = discord.Embed(
-                    title="ERROR", description="You have not accounts to list. Use /myaccs to add an account", color=discord.Color.red())
-            else:
-                embed = discord.Embed(
-                    title="Your Accounts", color=discord.Color.dark_red())
-                for account in author_accs:
-                    embed.add_field(name=account[2], value=account[1])
-        await ctx.respond(embed)
+        await ctx.respond(embed=await listHelper(
+            ctx=ctx,
+            type="M",
+            engine=engine
+        ))
 
     @myaccs.command()
     async def add(self, ctx, account: str, region: str, note: str):
-        CreateAccTable(engine, ctx.author.id, "M")
-        account, tag = account.split("#") if account != None else None
-        if region != None:
-            if account != None:
-                if note == None:
-                    note = "No note"
-                returned = AddAcc(
-                    engine, ctx.author.id, "M", f"{account}#{tag}", note)
-                if returned == "duplicate":
-                    embed = discord.Embed(
-                        title="ERROR", description="That account has already been saved by you", color=discord.Color.red())
-                elif returned == "maxed":
-                    embed = discord.Embed(title="ERROR", description="Listen man, you have 25 accounts saved. there is no reason you should have that many accounts. You should seriously reconsider whatever life decisions brought you here. Sorry but this is an intervention. If you got here by adding a bunch of other accounts then you're using this wrong. Alrigt rant over, have a good night or afternoon or day or whatever.",  color=discord.Color.red())
-                elif returned == "sucess":
-                    embed = discord.Embed(
-                        title="Sucess", description="Your account has successfully been added to the database", color=discord.Color.green())
-
-            else:
-                embed = discord.Embed(
-                    title="Error", description="Please specify a VALORANT account", color=discord.Color.red())
-
-        else:
-            embed = discord.Embed(
-                title="Error", description="Please specify a region", color=discord.Color.red())
-        await ctx.respond(embed)
+        await ctx.respond(embed=await addHelper(
+            ctx=ctx,
+            type="M",
+            engine=engine,
+            account=account,
+            region=region,
+            note=note
+        ))
 
     @myaccs.command()
     async def remove(self, ctx, account: str):
-        CreateAccTable(engine, ctx.author.id, "M")
-        account, tag = account.split("#") if account != None else None
-        returned = RmAcc(engine, ctx.author.id,
-                         "M", f"{account}#{tag}")
-        if returned == "sucess":
-            embed = discord.Embed(
-                title="Sucess", description="You account has successfully been removed from the database", color=discord.Color.green())
-        elif returned == "NIDB":
-            embed = discord.Embed(
-                title="ERROR", description="That account isn't in the database. You likely misspelled something", color=discord.Color.red())
-        await ctx.respond(embed)
+        await ctx.respond(embed=await removeHelper(
+            ctx=ctx,
+            type="M",
+            engine=engine,
+            account=account
+        ))
 
 
 # --------------------------------
 
+
     @quickaccs.command(name="list")
     async def list(self, ctx):
-        CreateAccTable(engine, ctx.author.id, "Q")
-        with engine.connect() as conn:
-            result = conn.execute(
-                text(f"SELECT * FROM Q{ctx.author.id}")
-            )
-            author_accs = result.all()
-
-        if len(author_accs) == 0:
-            embed = discord.Embed(
-                title="ERROR", description="You have not accounts to list. Use /quickaccs to add an account", color=discord.Color.red())
-        else:
-            embed = discord.Embed(
-                title="Your Quick Accounts", color=discord.Color.dark_red())
-            for account in author_accs:
-                embed.add_field(name=account[2], value=account[1])
-        await ctx.respond(embed)
+        await ctx.respond(embed=await listHelper(
+            ctx=ctx,
+            type="Q",
+            engine=engine
+        ))
 
     @quickaccs.command(name="add")
     async def qadd(self, ctx, account: str, note: str, region: str):
-        account, tag = account.split("#") if account != None else None
-        if region != None:
-            if account != None:
-                if note == None:
-                    note = "No note"
-                returned = AddAcc(
-                    ctx.author.id, "Q", f"{account}#{tag}", note)
-                if returned == "duplicate":
-                    embed = discord.Embed(
-                        title="ERROR", description="That account has already been saved by you", color=discord.Color.red())
-                elif returned == "sucess":
-                    embed = discord.Embed(
-                        title="Sucess", description="Your account has successfully been added to the database", color=discord.Color.green())
-
-            else:
-                embed = discord.Embed(
-                    title="Error", description="Please specify a VALORANT account", color=discord.Color.red())
-
-        else:
-            embed = discord.Embed(
-                title="Error", description="Please specify a region", color=discord.Color.red())
-        await ctx.respond(embed)
+        await ctx.respond(embed=await addHelper(
+            ctx=ctx,
+            type="Q",
+            engine=engine,
+            account=account,
+            region=region,
+            note=note
+        ))
 
     @quickaccs.command(name="remove")
     async def qremove(self, ctx, account: str):
-        account, tag = account.split("#") if account != None else None
-        returned = RmAcc(engine, ctx.author.id,
-                         "Q", f"{account}#{tag}")
-        if returned == "sucess":
-            embed = discord.Embed(
-                title="Sucess", description="You account has successfully been removed from the database", color=discord.Color.green())
-        elif returned == "NIDB":
-            embed = discord.Embed(
-                title="ERROR", description="That account isn't in the database. You likely misspelled something", color=discord.Color.red())
-        await ctx.respond(embed=embed)
+        await ctx.respond(embed=await removeHelper(
+            ctx=ctx,
+            type="Q",
+            engine=engine,
+            account=account
+        ))
 
 
 def setup(bot):
---
+++ helpers/accHelpers.py
@@ -0,0 +1,240 @@
+import discord
+from helpers.Helper import CreateAccTable, AddAcc, RmAcc, compTiers
+from sqlalchemy import engine, text
+import math
+import requests
+import logging
+import asyncio
+from PIL import ImageColor
+
+
+async def addHelper(ctx: discord.ApplicationContext, type: str, engine: engine.Engine,  account: str, region: str, note: str) -> discord.Embed:
+    CreateAccTable(engine, ctx.author.id, type)
+    try:
+        account, tag = account.split("#") if account != None else None
+    except ValueError:
+        return(discord.Embed(title="ERROR", description="Your account is missing a tag", color=discord.Color.red()))
+    if region != None:
+        if account != None:
+            if note == None:
+                note = "No note"
+            returned = AddAcc(
+                engine, ctx.author.id, type, f"{account}#{tag}", note, region)
+            if returned == "duplicate":
+                return(discord.Embed(
+                    title="ERROR", description="That account has already been saved by you", color=discord.Color.red()))
+            elif returned == "maxed":
+                return(discord.Embed(title="ERROR", description="Listen man, you have 25 accounts saved. Sorry but this is an intervention. Please reconsider your life choices leading up to here.",  color=discord.Color.red()))
+            elif returned == "sucess":
+                return(discord.Embed(
+                    title="Sucess", description="Your account has successfully been added to the database", color=discord.Color.green()))
+
+        else:
+            return(discord.Embed(
+                title="Error", description="Please specify a VALORANT account", color=discord.Color.red()))
+
+    else:
+        return(discord.Embed(
+            title="Error", description="Please specify a region", color=discord.Color.red()))
+
+
+async def removeHelper(ctx: discord.ApplicationContext, type: str, engine: engine.Engine, account: str):
+    CreateAccTable(engine, ctx.author.id, type)
+    account, tag = account.split("#") if account != None else None
+    returned = RmAcc(engine, ctx.author.id,
+                     type, f"{account}#{tag}")
+    if returned == "sucess":
+        return(discord.Embed(
+            title="Sucess", description="You account has successfully been removed from the database", color=discord.Color.green()))
+    elif returned == "NIDB":
+        return(discord.Embed(
+            title="ERROR", description="That account isn't in the database. You likely misspelled something", color=discord.Color.red()))
+
+
+async def listHelper(ctx: discord.ApplicationContext, type: str, engine: engine.Engine):
+    CreateAccTable(engine, ctx.author.id, type)
+    with engine.connect() as conn:
+        result = conn.execute(
+            text(f"SELECT * FROM {type}{ctx.author.id}")
+        )
+        author_accs = result.all()
+
+        if len(author_accs) == 0:
+            embed = discord.Embed(
+                title="ERROR", description="You have not accounts to list. Use /myaccs to add an account", color=discord.Color.red())
+        else:
+            embed = discord.Embed(
+                title="Your Accounts" if type != "Q" else "Your Quick Accounts", color=discord.Color.dark_red())
+            for account in author_accs:
+                embed.add_field(name=account[2], value=account[1])
+        return(embed)
+
+
+async def getAccFromList(ctx: discord.ApplicationContext, bot: discord.bot.Bot, operation: str, engine: engine.Engine):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text(f"SELECT * FROM {operation}{ctx.author.id}"))
+        accounts = result.all()  # 5 accounts per page with a left and right arrow
+        page = 1
+        max_page_count = math.ceil(len(accounts)/5)
+        niceType = "quick" if operation == "Q" else "my"
+
+        embed = discord.Embed(
+            title=f"Your {niceType}accounts list", color=discord.Color.red(), description=None)
+        embed.set_footer(
+            text=f"Page {page} / {max_page_count} \n Razebot by MaximumMaxx")
+        await ctx.respond(embed=embed)
+        message = await ctx.interaction.original_message()
+        # getting the message object for editing and reacting
+
+        await message.add_reaction("◀️")
+        await message.add_reaction("1️⃣")
+        await message.add_reaction("2️⃣")
+        await message.add_reaction("3️⃣")
+        await message.add_reaction("4️⃣")
+        await message.add_reaction("5️⃣")
+        await message.add_reaction("▶️")
+        number_reactions = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"]
+
+        def check(reaction, user):
+            return user == ctx.author
+            # This makes sure nobody except the command sender can interact with the "menu"
+
+        # Getting which account the user wants
+        try:
+            while True:
+                reaction, user = await bot.wait_for("reaction_add", timeout=60, check=check)
+                # waiting for a reaction to be added - times out after 60 seconds
+
+                if str(reaction.emoji) == "▶️" and page != max_page_count:
+                    page += 1
+                    embed = discord.Embed(
+                        title=f"Your {list}accounts list", color=discord.Color.red(), description=None)
+                    embed.set_footer(
+                        text=f"Page {page} / {max_page_count} \n Razebot by MaximumMaxx")
+                    for i in range(5):
+                        j = page*5 + i
+                        if not j > len(accounts):
+                            embed.add_field(embed.add_field(
+                                name=f"{j+1}) {accounts[j][2]}", value=accounts[j][1]))
+                    await message.edit(embed=embed)
+                    await message.remove_reaction(reaction, user)
+
+                elif str(reaction.emoji) == "◀️" and page > 1:
+                    page -= 1
+                    embed = discord.Embed(
+                        title=f"Your {list}accounts list", color=discord.Color.red(), description=None)
+                    embed.set_footer(
+                        text=f"Page {page} / {max_page_count} \n Razebot by MaximumMaxx")
+                    for i in range(5):
+                        j = page*5 + i
+                        if not j > len(accounts):
+                            embed.add_field(embed.add_field(
+                                name=f"{j+1}) {accounts[j][2]}", value=accounts[j][1]))
+                    await message.edit(embed=embed)
+                    await message.remove_reaction(reaction, user)
+
+                elif str(reaction.emoji) in number_reactions:
+                    account_index = page*5 + \
+                        number_reactions.index(str(reaction.emoji)) + 1
+                    number_reactions.index(str(reaction.emoji))
+                    if account_index - 5 > len(accounts):
+                        await message.remove_reaction(reaction, user)
+                    else:
+                        break
+
+                else:
+                    await message.remove_reaction(reaction, user)
+                    # removes reactions if the user tries to go forward on the last page or
+                    # backwards on the first page
+
+            # The -6 is really arbitary and honestly I have no idea why it's needed but it makes it work
+            await ctx.edit(content="Working on that ... please wait", embed=None)
+            return((get_acc(accounts[account_index-6]), message))
+
+        # If the user doens't respond withing 60 seconds
+        except asyncio.TimeoutError:
+            embed = discord.Embed(title=f"ERROR", color=discord.Color.red(
+            ), description="Your time to select an items has timed out. Please try again.")
+            embed.set_footer(text="Razebot by MaximumMaxx")
+            return(embed, message)
+
+
+def get_acc(account: str) -> discord.Embed:
+    # Getting the actual account details
+    acc, tag = account[2].split("#")
+    region = account[3]
+    try:
+        accountReq = requests.get(
+            f"https://api.henrikdev.xyz/valorant/v1/account/{acc}/{tag}")
+        if accountReq.status_code != 200:
+            return(discord.Embed(title="ERROR", description="Account not found"))
+        else:
+            puuid = accountReq.json()["data"]['puuid']
+    except discord.ApplicationCommandInvokeError or KeyError as exception:
+        logging.exception(
+            f"Failed to retrieve account details with url: https://api.henrikdev.xyz/valorant/v1/account/{acc}/{tag} produces error: {exception}")
+        return(discord.Embed(title="ERROR", description="Account not found", color=discord.Color.red()))
+    MH = requests.get(
+        f"https://api.henrikdev.xyz/valorant/v3/matches/{region}/{acc}/{tag}?filter=competitive")
+    MMR = requests.get(
+        f"https://api.henrikdev.xyz/valorant/v1/mmr-history/{region}/{acc}/{tag}")
+
+    kills = deaths = assists = score = mmr_change = "Unknown"
+
+    if MMR.status_code == 200:
+        MMR_json = MMR.json()
+        mmr_change = 0
+        for game in MMR_json["data"]:
+            mmr_change += game["mmr_change_to_last_game"]
+        if len(MMR_json["data"]) == 0:
+            return discord.Embed(title="ERROR", description="No data avaliable for that player. Double check your spelling and try again.", color=discord.Color.red())
+        rank = MMR_json["data"][0]["currenttierpatched"]
+        tiernum = MMR_json["data"][0]["currenttier"]
+        elo = MMR_json["data"][0]["elo"]
+        image: str = compTiers().json(
+        )["data"][0]["tiers"][tiernum]["largeIcon"]
+        color = compTiers().json(
+        )["data"][0]["tiers"][tiernum]["color"][:6]
+        embed = discord.Embed(color=discord.Color.from_rgb(ImageColor.getcolor("#"+color, "RGB")[0], ImageColor.getcolor(
+            "#"+color, "RGB")[1], ImageColor.getcolor("#"+color, "RGB")[2]), description=f"The stats and rank for {acc}", title=acc)
+        embed.add_field(name="Rank", value=rank)
+        embed.add_field(name="MMR", value=elo)
+    elif MMR.status_code == 204:
+        return(discord.Embed(title="ERROR", description="Not enough recent data or wrong region", color=discord.Color.red()))
+    elif MMR.status_code == 429:
+        return(discord.Embed(title="ERROR", description="The bot has been rate limited. Please try again in a few minutes", color=discord.Color.red()))
+    else:
+        logging.error(
+            f"{MMR.json()} error while retrieve data region: {region} username: {acc} tag: {tag}")
+        return(discord.Embed(title="ERROR", description="Something went horribly wrong and we are all going to die.", color=discord.Color.red()))
+
+    if MH.status_code == 200:
+        MH_json = MH.json()
+        match_count = len(MH_json["data"])
+        kills, deaths, assists, score = 0, 0, 0, 0
+
+        for i in range(match_count):
+            for j in range(len(MH_json["data"][i]["players"]["all_players"])):
+                if MH_json["data"][i]["players"]["all_players"][j]["puuid"] == puuid:
+                    player_index = j
+                    break
+            kills += MH_json["data"][i]["players"]["all_players"][player_index]["stats"]["kills"]
+            deaths += MH_json["data"][i]["players"]["all_players"][player_index]["stats"]["deaths"]
+            assists += MH_json["data"][i]["players"]["all_players"][player_index]["stats"]["assists"]
+            score += MH_json["data"][i]["players"]["all_players"][player_index]["stats"]["score"]
+        kills = round(kills/match_count, 1)
+        deaths = round(deaths/match_count, 1)
+        assists = round(assists/match_count, 1)
+        score = round(score / match_count, 1)
+
+        embed.add_field(name="Average stats from the last 5 games",
+                        value=f"KDA: {kills} | {deaths} | {assists} \n KDR: {round(kills/deaths,2)} \n Score: {score} \n MMR Change: {mmr_change}")
+    else:
+        embed.add_field(
+            name="ERROR", value="Error getting other stats. If the issue persists please contact me @ MaximumMaxx#0001")
+        logging.error(
+            f"Failure retrieving Match History data: {MH.json()}")
+    embed.set_image(url=image)
+    embed.set_footer(text="Razebot by MaximumMaxx")
+    return(embed)
